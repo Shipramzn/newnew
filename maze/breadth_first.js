a16d36c9b16ae8dba5261ac357eb47c180326dfc
Plaksha05 <63656322+Plaksha05@users.noreply.github.com>
2020-07-22 19:01:25 +0530

    Update breadth_first.js

--- maze/breadth_first.js
+++ maze/breadth_first.js
@@ -29,42 +29,11 @@ function solve_using_bfs(){
     var time_e = new Date().getTime();
 
     if(flag ===1)
-    {  
-        var i=0
-        var j=0
-        path.reverse()
-        
-        let timerId= window.setInterval(function(){
-            if(i === visited_elt.length){
-                i=0
-            }else{
-                if(!path.includes(visited_elt[i])){
-                    visited_elt[i].state='p'
-                    tiles[sc][sr].state='s'
-                    tiles[ec][er].state='f'
-                }
-            }
-            i++
-        }, 10)
+    { 
+        var time= time_e-time_s
+        closed(visited_elt,path)
+        pathvisual(path,time,visited_elt)
 
-        var len= length(path).toFixed(2)
-
-        setTimeout(() => { clearInterval(timerId); console.log('solution exists'); }, visited_elt.length * 20);
-        let timerId1= window.setInterval(function(){
-            if(j=== path.length){
-                j=0
-                document.getElementById("outcome").innerHTML = `Length= ${len} <br> Time= ${(time_e-time_s).toFixed(3)}ms`;
-
-                // break
-
-            }else{
-                path[j].state='x'
-                tiles[sc][sr].state='s'
-                tiles[ec][er].state='f'
-            }       
-            j++
-        }, (visited_elt.length * 11)/(path.length))
-        setTimeout(() => { clearInterval(timerId1); console.log('solution exists'); }, visited_elt.length * 20);
 
     }else{
         console.log('Solution does not exist')
