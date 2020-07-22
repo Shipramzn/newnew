9994dc4426b32940308dcb1fff9557731a65ad32
Plaksha05 <63656322+Plaksha05@users.noreply.github.com>
2020-07-22 19:00:51 +0530

    Update best_first.js

--- maze/best_first.js
+++ maze/best_first.js
@@ -60,78 +60,16 @@ function solve_using_bestfirst(heu){
             var time_e = new Date().getTime();
 
             
-        }       
-            
-             
+        }                    
             
             if(flag === 0){
                 console.log('No path exists')
             }else{
-                console.log('Path exists')    
-               var i=0
-               var j=0
-               var k=0
-               path.reverse()
-              let timerId= window.setInterval(function(){
-                if(i === closedList.length){
-                    i=0
-                    clearInterval(timerId)
-                }else{
-                    if(!path.includes(closedList[i])){
-                        closedList[i].state='p'
-                        tiles[sc][sr].state='s'
-                        tiles[ec][er].state='f'
-                       // openSet[i].state ='b'
-               }
-                }
-              
-             i++
-             
-               
-         }, 10)
-        //  setTimeout(() => { clearInterval(timerId)}, closedList.length * 100);
-         let timerId1= window.setInterval(function(){
-          
-           if(j === openList.length){
-               j=0
-               clearInterval(timerId1)
-           }else{
-            if(!path.includes(openList[j])){
-                openList[j].state ='l'
-                tiles[sc][sr].state='s'
-                tiles[ec][er].state='f'
-               
-             }
-           }
-           
-            
-   
-       j++
-       
-         
-   }, (closedList.length*11)/(openList.length))
-   var len= length(path).toFixed(2)
-//    setTimeout(() => { clearInterval(timerId1) }, closedList.length * 100);
-   let timerId2= window.setInterval(function(){
-     if(k=== path.length){
-         k=0
-         document.getElementById("outcome").innerHTML = `Length= ${len} <br> Time= ${(time_e-time_s).toFixed(3)}ms`;
-         clearInterval(timerId2)
-     }else{
-        path[k].state='x'
-        tiles[sc][sr].state='s'
-        tiles[ec][er].state='f'
-       // openSet[i].state ='b'
-
-     }
-    
-        
-   k++
-   
-     
-}, (closedList.length*11)/(path.length))
-// setTimeout(() => { clearInterval(timerId2)}, closedList.length * 100);
-
+                
+                var time= time_e-time_s
+                closed(closedList,path)
+                open(openList,closedList,path)
+                pathvisual(path,time,closedList)
 
   }
 }
