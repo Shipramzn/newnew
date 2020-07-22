1a34499f15befd0339a782c89af3e6feac08bfd9
Shipramzn <67088997+Shipramzn@users.noreply.github.com>
2020-07-22 21:04:55 +0530

    Update grid.js

--- maze/grid.js
+++ maze/grid.js
@@ -1,6 +1,6 @@
 const canvas= document.getElementById("myCanvas");
 const ctx= canvas.getContext("2d");
-canvas.height = window.innerHeight-172
+canvas.height = window.innerHeight-157
 canvas.width = window.innerWidth -15
 const Height= canvas.height;
 const Width= canvas.width;
@@ -29,10 +29,10 @@ var start
 var end
 var X
 var Y
-tileW= 25;          // width of each tile
-tileH= 25;          // height of each tile
-tileRow=(Height/28);        // number of rows in grid
-tileColumn= (Width/28);                     // number of columns in grid
+tileW= 20;          // width of each tile
+tileH= 20;          // height of each tile
+tileRow=(Height/23);        // number of rows in grid
+tileColumn= (Width/23);                     // number of columns in grid
 boundX=0;
 boundY=0;
 
